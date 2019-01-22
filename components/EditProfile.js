a3feff11544c101c06ee3bd84845b57f96abd5fd
RovertJonybal <jonybalsony@outlook.com>
2019-01-23 00:04:07 +0800

    removed commented line

--- components/EditProfile.js
+++ components/EditProfile.js
@@ -303,7 +303,6 @@ class EditProfile extends Component {
                       onChangeText={username => this.setState({ username })}
                     />
                   </Button>
-                  {/* <Text style={styles.thirdText}>Fees: {Store.doctorProfile.fees} KD</Text> */}
                 </Icon>
               </Col>
               <Col style={{ marginTop: 20 }}>
