6fe2e2be53d2b183fdfe039f43ea4de2198ab641
Ahmed Alnjjar <aalnjjar32@gmail.com>
2019-07-16 00:41:18 +0300

    upgrade-up
    up runing

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,5 +1,12 @@
-const blacklist = require("metro/src/blacklist");
+// const blacklist = require("metro/src/blacklist");
 
+// module.exports = {
+//   getBlacklistRE() {
+//     return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/]);
+//   }
+// };
+
+const blacklist = require("metro-config/src/defaults/blacklist");
 module.exports = {
   getBlacklistRE() {
     return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/]);
